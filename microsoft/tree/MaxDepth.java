0d2b3293dadfd7192e59bf378b54dbcd9c9ad68e
SanleyPan <786836525@qq.com>
2022-05-23 21:53:50 +0800

    104. Maximum Depth of Binary Tree

---
+++ microsoft/tree/MaxDepth.java
@@ -0,0 +1,38 @@
+package tree;
+
+public class MaxDepth {
+
+
+
+  //用后续遍历的思想 当node 为空时返回 0 ，不然返回1 ，一层一层从底向上加
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int leftDepth = maxDepth(root.left);
+    int rightDepth = maxDepth(root.right);
+    return 1 + Math.max(leftDepth, rightDepth);
+  }
+
+
+
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
